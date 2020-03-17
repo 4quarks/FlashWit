db4522829cdb179f1cf50eb99e7c14766872e075
paucutrina <paucutrina@gmail.com>
2020-03-17 10:47:34 +0100

    scrapper streaming ready

--- scrappers/Utils.py
+++ scrappers/Utils.py
@@ -1,52 +1,98 @@
 from selenium import webdriver
 import os
 from bs4 import BeautifulSoup
-from scrappers.constants_scrappers import Constants
+from scrappers.constants_scrappers import Constants, FileNames
 import time
+import logging
+import json
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
-def load_web_driver():
-    path_webdrivers = os.path.dirname(os.path.realpath(__file__)) + "/"
-    os.environ["PATH"] += ";" + path_webdrivers
-    print('AAA' + path_webdrivers)
+
+def load_browser():
+    ############# GET WEB DRIVERS PATH #############
+    directory_path = os.path.dirname(os.path.realpath(__file__))
+    path_webdrivers = directory_path + "/" + FileNames.WEBDRIVER_UBUNTU
+
+    ############# LOAD BROWSER #############
     browser_options = webdriver.ChromeOptions()
-    browser = webdriver.Chrome(path_webdrivers + "chromedriver", chrome_options=browser_options)
-
+    browser = webdriver.Chrome(path_webdrivers, chrome_options=browser_options)
     return browser
 
 
-def load_web_scraper(site):
-    browser = load_web_driver()
-    browser.refresh()
+def get_web_html(browser, site):
     browser.get(site)
+    page_source = browser.page_source
+    web_html = BeautifulSoup(page_source, 'html.parser')
+    return web_html
+
+
+def refresh_browser(browser):
+    browser.refresh()
     browser.execute_script("window.scrollTo(0, 0)")
-    return browser.page_source
 
 
-if __name__ == "__main__":
+def get_text_and_clean(bs4_list):
+    return bs4_list[0].get_text().strip()
+
+
+def scrap_web(browser):
+    ############# GET WEBSITE'S HTML #############
+    web_html = get_web_html(browser, Constants.WEBSITE)
+
+    ############# FIND GENERAL TABLES OF CONTENT #############
     dict_result_scrapping = {}
-    response = load_web_scraper(Constants.WEBSITE)
-    soup = BeautifulSoup(response, 'html.parser')
-    # scraped_data = soup.findAll('tr', {'class': ['datatable-row', "datatable-row-alternating"]})
-    tables = soup.findAll('div', {'class': "table-responsive"})
+    tables = web_html.findAll('div', {'class': "table-responsive"})
+
+    ############# GET ROWS FROM EACH TABLE #############
     for country_market_bs4 in tables:
         row_head = country_market_bs4.findAll('thead')
+        markets = country_market_bs4.findAll('tr', {'class': ['datatable-row', "datatable-row-alternating"]})
+
+        # Get country from the first row (header row)
+        country = row_head[0].findAll('th', {'style': "text-align: left;cursor: pointer;"})
+        country_name = get_text_and_clean(country)
+
         # headers = row_head[0].get_text().strip().split('\n')
         # clean_headers = [element.strip() for element in headers if element.strip() != '']
-        country = row_head[0].findAll('th', {'style': "text-align: left;cursor: pointer;"})
-        country_name = country[0].get_text().strip()
+
         dict_result_scrapping[country_name] = {}
-        print('Country: ' + country_name)
-        markets = country_market_bs4.findAll('tr', {'class': ['datatable-row', "datatable-row-alternating"]})
+        logging.debug('Country: ' + country_name)
+
+        ############# GET SPECIFIC VALUES FOR EACH MARKET #############
         for row_market_bs4 in markets:
             market_name_bs4 = row_market_bs4.findAll('td', {'class': 'datatable-item-first'})
-            market_name = market_name_bs4[0].get_text().strip()
-            print('Market name: ' + market_name)
-            dict_result_scrapping[country_name][market_name] = {}
             value_market = row_market_bs4.findAll('td', {'class': 'datatable-item'})
+
+            # Get all the values from each row
+            market_name = get_text_and_clean(market_name_bs4)
+
+            dict_result_scrapping[country_name][market_name] = {}
+            logging.debug('Market name: ' + market_name)
+
+            ############# CLEAN VALUES #############
             for index, value in enumerate(value_market):
                 clean_value = value.get_text().strip()
                 dict_result_scrapping[country_name][market_name][Constants.TAGS[index]] = clean_value
-                print(Constants.TAGS[index] + ': ' + clean_value)
-            print('*'*10)
-    print()
+
+                logging.debug(Constants.TAGS[index] + ': ' + clean_value)
+
+            logging.debug('*' * 10)
+
+    ############# JSON WITH ALL DATA COLLECTED #############
+    json_result = json.dumps(dict_result_scrapping)
+    return json_result
+
+
+def launch_scrapper():
+    ############# OPEN BROWSER #############
+    browser = load_browser()
+    while True:  # Don't close the browser
+        json_response = scrap_web(browser)
+        # -----------> KAFKA connected with the json_response
+        time.sleep(Constants.FREQUENCY_SCRAPPING_MS / 1000)
+    # browser.quit()
+
+
+if __name__ == "__main__":
+    launch_scrapper()
 
--- scrappers/constants_scrappers.py
+++ scrappers/constants_scrappers.py
@@ -1,3 +1,12 @@
 class Constants:
     TAGS = ['Price', 'Value', 'Daily', 'Weekly', 'Monthly', 'Yearly', 'Date']
     WEBSITE = "https://tradingeconomics.com/stocks"
+    FREQUENCY_SCRAPPING_MS = 100
+
+
+class FileNames:
+    WEBDRIVER_UBUNTU = "chromedriver"
+
+
+
+
